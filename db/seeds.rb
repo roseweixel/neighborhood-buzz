dc8682142ec92cbac1d09759d820d585ca0ee0bb
amshane <allisonmandy90@gmail.com>
2014-11-19 16:43:53 -0500

    fixed seeds and got an api key

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,30 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-neighborhoods = Neighborhood.create[{name: 'Lower Manhattan'}, {name: 'Tribeca'}, {name: 'Soho'}, {name: 'Greenwich Village'}, {name: 'East Village'}, {name: 'Lower East Side'}, {name: 'Nolita and Noho'}, {name: 'Gramercy Park and Murray Hill'}, {name: 'Chelsea'}, {name: 'Midtown East'}, {name: 'Midtown West and Hells Kitchen'}, {name: 'Upper East Side'}, {name: 'Upper West Side'}, {name: 'Morningside Heights'}, {name: 'Harlem'}, {name: 'Hamilton Heights and Washington Heights'}, {name: 'Brooklyn Heights and Cobble Hill'}, {name: 'Boerum Hill and Carroll Gardens'}, {name: 'Red Hook'}, {name: 'Dumbo'}, {name: 'Fort Greene and Clinton Hill'}, {name: 'Park Slope'}, {name: 'Beyond the Slope'}, {name: 'Williamsburg and Greenpoint'}, {name: 'Astoria and Long Island City'}]
+neighborhoods = Neighborhood.create([
+    {name: 'Lower Manhattan'}, 
+    {name: 'Tribeca'}, 
+    {name: 'Soho'}, 
+    {name: 'Greenwich Village'}, 
+    {name: 'East Village'}, 
+    {name: 'Lower East Side'}, 
+    {name: 'Nolita and Noho'}, 
+    {name: 'Gramercy Park and Murray Hill'}, 
+    {name: 'Chelsea'}, 
+    {name: 'Midtown East'}, 
+    {name: 'Midtown West and Hells Kitchen'}, 
+    {name: 'Upper East Side'}, 
+    {name: 'Upper West Side'}, 
+    {name: 'Morningside Heights'}, 
+    {name: 'Harlem'}, 
+    {name: 'Hamilton Heights and Washington Heights'}, 
+    {name: 'Brooklyn Heights and Cobble Hill'}, 
+    {name: 'Boerum Hill and Carroll Gardens'}, 
+    {name: 'Red Hook'}, 
+    {name: 'Dumbo'}, 
+    {name: 'Fort Greene and Clinton Hill'}, 
+    {name: 'Park Slope'}, 
+    {name: 'Beyond the Slope'}, 
+    {name: 'Williamsburg and Greenpoint'}, 
+    {name: 'Astoria and Long Island City'}
+    ])
