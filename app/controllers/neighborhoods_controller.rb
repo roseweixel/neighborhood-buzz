ce42b00fdd63a343c85d53ea082d145b89e478fc
amshane <allisonmandy90@gmail.com>
2014-11-20 16:42:15 -0500

    added dropdown menu

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -1,11 +1,16 @@
 class NeighborhoodsController < ApplicationController
     def index
         @neighborhoods = Neighborhood.all
+        @neighborhood = Neighborhood.new
     end
 
     def show
         @neighborhood = Neighborhood.find(params[:id])
         @noko = @neighborhood.noko_listing
+    end
+
+    def create
+        redirect_to neighborhood_path(params[:neighborhood][:id])
     end
 
 end
