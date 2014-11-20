b1036e21474fa7f1fbd78e2a40afb51304062216
mhfairbank <mhfairbank@gmail.com>
2014-11-20 14:05:26 -0500

    build street easy api functionality

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -5,6 +5,7 @@ class Neighborhood < ActiveRecord::Base
 
 # API_KEY = "019d22d7db27f8ef0242efc21d8d645e:19:70210388"
     API_KEY = "019d22d7db27f8ef0242efc21d8d645e%3A19%3A70210388"
+    SE_API_KEY = "a4e83294da970c9775f1b4e35dbbe1f87299c242"
 
     def urlified_name
         name.gsub(" ", "+")
@@ -29,5 +30,13 @@ class Neighborhood < ActiveRecord::Base
         info_hash["results"][0]["listing_price"]
     end
  
+    def find_se_rentals
+        url = "http://streeteasy.com/nyc/api/rentals/data?criteria=area:#{urlified_name}-#{borough}\|beds:2&key=#{SE_API_KEY}&format=json"
+        encoded_url = URI.encode(url)
+        info_hash = JSON.load(open(encoded_url))
+  
+        {median_price: info_hash["median_price"], search_url: info_hash["search_url"]}
+    end
+
 
 end
