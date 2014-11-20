35306665ec3ed9b81109a1155a4c1bd849e4eeb2
amshane <allisonmandy90@gmail.com>
2014-11-20 12:04:04 -0500

    added date function to percentile

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,19 +1,31 @@
 require 'open-uri'
+require 'date'
 
 class Neighborhood < ActiveRecord::Base
 
-# API_KEY = 019d22d7db27f8ef0242efc21d8d645e:19:70210388
+# API_KEY = "019d22d7db27f8ef0242efc21d8d645e:19:70210388"
     API_KEY = "019d22d7db27f8ef0242efc21d8d645e%3A19%3A70210388"
 
     def urlified_name
         name.gsub(" ", "+")
     end
 
+    def get_week
+        week = Date.today.cweek
+        if week == 53
+            week = 52
+        end
+        week
+    end
+
+    def get_year
+        Date.today.year
+    end
+
     def find_50th_percentile
         type = "json"
-        url = "http://api.nytimes.com/svc/real-estate/v2/listings/percentile/50.#{type}?date-range=2014-10&geo-extent-level=neighborhood&geo-extent-value=#{urlified_name}&api-key=#{API_KEY}"
+        url = "http://api.nytimes.com/svc/real-estate/v2/listings/percentile/50.#{type}?date-range=#{get_year}-W#{get_week}&geo-extent-level=neighborhood&geo-extent-value=#{urlified_name}&api-key=#{API_KEY}"
         info_hash = JSON.load(open(url))
-        puts info_hash.inspect
     end
  
 
