d3440056a7adb54fe17214f455dbb3ed2798c86b
mhfairbank <mhfairbank@gmail.com>
2014-11-21 11:34:26 -0500

    build further login signup functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    user = User.find_by(email: params[:email])
+    user = User.find_by(username: params[:username])
     if user
       session[:user_id] = user.id
       redirect_to neighborhoods_path
