6f0ddc03c7d368f9943654bb123b3f816eb6ba28
Rose Weixel <rose.weixel@gmail.com>
2014-11-21 12:28:40 -0500

    make neighborhood show page work for signed in user

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -12,6 +12,11 @@ class NeighborhoodsController < ApplicationController
     def show
         @neighborhood = Neighborhood.find(params[:id])
         @noko = @neighborhood.noko_listing
+        if session[:user_id]
+            @user = User.find(session[:user_id])
+        else
+            @user = User.new
+        end
     end
 
     def create
