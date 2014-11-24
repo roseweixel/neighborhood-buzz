7d30a46d3a2e77de1682fcaa61c78e369e05ae0a
amshane <allisonmandy90@gmail.com>
2014-11-23 19:43:55 -0500

    added google map with neighboorhood

--- app/assets/javascripts/neighborhoods.js
+++ app/assets/javascripts/neighborhoods.js
@@ -1,4 +1,5 @@
 // Ajaxify the favorites button!
+'use strict;'
 
 $(function(){
 	$('.favorite-button-wrapper').on("submit", "form", function(event){
@@ -13,3 +14,34 @@ $(function(){
 		});
 	});
 });
+
+
+function GMap(){
+  this.$map = $('#map_canvas');
+  this.address = $('.neighborhood-name').text();
+};
+
+GMap.prototype.insertMap = function(latitude, longitude){
+   var canvas = this.$map[0];
+   var myLatlng = new google.maps.LatLng(latitude, longitude);
+   var mapOptions = {
+     center: myLatlng,
+     zoom: 13,
+     mapTypeId: google.maps.MapTypeId.ROADMAP
+   }
+   var map = new google.maps.Map(canvas, mapOptions);
+   var marker = new google.maps.Marker({
+     position: myLatlng,
+     map: map
+   });
+};
+
+GMap.prototype.codeAddress = function(neighborhood){
+   var geocoder = new google.maps.Geocoder();
+   var address = neighborhood + " New York, NY";
+   geocoder.geocode( {'address': address}, function(results, status) {
+           var lat = results[0].geometry.location.lat();
+           var lon = results[0].geometry.location.lng();
+           this.insertMap(lat, lon);
+       }.bind(this));
+};
