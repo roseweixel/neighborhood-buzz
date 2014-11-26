b353eab73254001fe45ee6f6797577b825332aaa
Rose Weixel <rose.weixel@gmail.com>
2014-11-26 10:03:12 -0500

    fix some bugs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,17 +3,17 @@ class SessionsController < ApplicationController
     user = User.find_by(slug: sluggify(params[:username]))
     if user
       session[:user_id] = user.id
-      redirect_to(:back)
+      redirect_to root_path
     else
       flash[:notice] = "Could not find user!"
-      redirect_to(:back)
+      redirect_to root_path
     end
   end
 
   def destroy
     session[:user_id] = nil
     flash[:notice] = "You have been logged out."
-    redirect_to(:back)
+    redirect_to root_path
   end
 
 end
